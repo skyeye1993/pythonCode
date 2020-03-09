3b92a6904b7a3864f0b2e2017b5bd1263e24d498
XuXianda <1114186632@qq.com>
2020-03-09 11:55:39 +0800

    机器学习第二天

--- machineLearning/machine_learning_02.py
+++ machineLearning/machine_learning_02.py
@@ -6,7 +6,14 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
+
+pd.set_option('display.max_rows', 10000)
+pd.set_option('display.max_columns', 100)
+pd.set_option('display.width', 1000)
 
 
 def knn_iris():
@@ -149,11 +156,116 @@ def nb_new():
     x_train, x_test, y_train, y_test = train_test_split(news.data, news.target)
     # 3、特征工程：文本特征抽取-tfidf
     transfer = TfidfVectorizer()
-    x_train =transfer.fit_transform(x_train)
+    x_train = transfer.fit_transform(x_train)
     x_test = transfer.transform(x_test)
     # 4、朴素贝叶斯算法预估器流程
     estimator = MultinomialNB()
-    estimator.fit(x_train,y_train)
+    estimator.fit(x_train, y_train)
+    # 5、模型评估
+    # 方法1：直接比对真实值和预测值
+    y_predict = estimator.predict(x_test)
+    print('y_predict:\n', y_predict)
+    print('直接对比真实值和预测值：\n', y_test == y_predict)
+
+    # 方法2：计算准确率
+    score = estimator.score(x_test, y_test)
+    print("准确率为：\n", score)
+
+
+def decision_iris():
+    """
+    用决策树对鸢尾花进行分类
+    :return:
+    """
+    # 1、获取数据集
+    iris = load_iris()
+    # 2、划分数据集
+    x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, random_state=22)
+
+    # 3、决策树评估器
+    estimator = DecisionTreeClassifier(criterion='entropy')
+    estimator.fit(x_train, y_train)
+
+    # 4、模型评估
+    # 方法1：直接比对真实值和预测值
+    y_predict = estimator.predict(x_test)
+    print('y_predict:\n', y_predict)
+    print('直接对比真实值和预测值：\n', y_test == y_predict)
+
+    # 方法2：计算准确率
+    score = estimator.score(x_test, y_test)
+    print("准确率为：\n", score)
+
+    # 可视化决策树
+    export_graphviz(estimator, out_file='iris_tree.dot', feature_names=iris.feature_names)  # http://webgraphviz.com/
+
+
+def titanic_demo():
+    # 1、获取数据
+    path = 'http://biostat.mc.vanderbilt.edu/wiki/pub/Main/DataSets/titanic.txt'
+    titanic = pd.read_csv(path)
+    x = titanic[['pclass', 'age', 'sex']]
+    y = titanic['survived']
+
+    # 2、数据处理
+    # 1）缺失值处理
+    x['age'].fillna(x['age'].mean(), inplace=True)
+    # 2）转换成字典
+    x = x.to_dict(orient='records')
+    # 3）数据集划分
+    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=22)
+    # 4）字典特征抽取
+    transfer = DictVectorizer()
+    x_train = transfer.fit_transform(x_train)
+    x_test = transfer.transform(x_test)
+
+    # 3、决策树评估器
+    estimator = DecisionTreeClassifier(criterion='entropy', max_depth=8)
+    estimator.fit(x_train, y_train)
+    # 方法1：直接比对真实值和预测值
+    y_predict = estimator.predict(x_test)
+    print('y_predict:\n', y_predict)
+    print('直接对比真实值和预测值：\n', y_test == y_predict)
+
+    # 方法2：计算准确率
+    score = estimator.score(x_test, y_test)
+    print("准确率为：\n", score)
+
+    # 可视化决策树
+    export_graphviz(estimator, out_file='titanic_tree.dot',
+                    feature_names=transfer.get_feature_names())  # http://webgraphviz.com/
+
+
+# 随机森林
+def titanic_tree_demo():
+    # 1、获取数据
+    path = 'http://biostat.mc.vanderbilt.edu/wiki/pub/Main/DataSets/titanic.txt'
+    titanic = pd.read_csv(path)
+    x = titanic[['pclass', 'age', 'sex']]
+    y = titanic['survived']
+
+    # 2、数据处理
+    # 1）缺失值处理
+    x['age'].fillna(x['age'].mean(), inplace=True)
+    # 2）转换成字典
+    x = x.to_dict(orient='records')
+    # 3）数据集划分
+    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=22)
+    # 4）字典特征抽取
+    transfer = DictVectorizer()
+    x_train = transfer.fit_transform(x_train)
+    x_test = transfer.transform(x_test)
+
+    # 随机森林对泰坦尼克号乘客的生存预测
+    estimator = RandomForestClassifier()
+
+    # 加入网格搜索和交叉验证
+    # 参数准备
+    param_dict = {'n_estimators': [120, 200, 300, 500, 800, 1200],
+                  'max_depth': [5, 8, 15]}
+    estimator = GridSearchCV(estimator, param_grid=param_dict, cv=3)
+    estimator.fit(x_train, y_train)
+
     # 5、模型评估
     # 方法1：直接比对真实值和预测值
     y_predict = estimator.predict(x_test)
@@ -178,4 +290,7 @@ if __name__ == '__main__':
     # knn_iris()
     # knn_iris_gscv()
     # facebook()
-    nb_new()
+    # nb_new()
+    # decision_iris()
+    # titanic_demo()
+    titanic_tree_demo()
